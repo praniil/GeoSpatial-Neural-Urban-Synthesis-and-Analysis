fe0dc27174f05d690da080de19fd5585ed60b3ad
praniil <pparajuli15@gmail.com>
2025-10-20 19:26:24 +0545

    feat: seperated the image and its mask into different folders

--- src/finetuning_segformer/seperate_img_and_mask.py
+++ src/finetuning_segformer/seperate_img_and_mask.py
@@ -16,32 +16,5 @@ for fname in os.listdir(source_dir_train):
     elif fname.endswith("_mask.png"):
         shutil.move(os.path.join(source_dir_train, fname), os.path.join(mask_dir_train, fname))
 
-#seperate_test
-source_dir_test = "../../datasets/deepGlobe Land Cover Classification Dataset/test"
-image_dir_test = f"{source_dir_test}/image"
-mask_dir_test = f"{source_dir_test}/mask"
 
-os.makedirs(image_dir_test, exist_ok=True)
-os.makedirs(mask_dir_test, exist_ok=True)
-
-for fname in os.listdir(source_dir_test):
-    if fname.endswith("_sat.jpg"):
-        shutil.move(os.path.join(source_dir_test,fname), os.path.join(image_dir_test, fname))
-    elif fname.endswith("_mask.png"):
-        shutil.move(os.path.join(source_dir_test, fname), os.path.join(mask_dir_test, fname))
-
-#seperate_valid
-source_dir_valid = "../../datasets/deepGlobe Land Cover Classification Dataset/valid"
-image_dir_valid = f"{source_dir_valid}/image"
-mask_dir_valid = f"{source_dir_valid}/mask"
-
-
-os.makedirs(image_dir_valid, exist_ok=True)
-os.makedirs(mask_dir_valid, exist_ok=True)
-
-for fname in os.listdir(source_dir_valid):
-    if fname.endswith("_sat.jpg"):
-        shutil.move(os.path.join(source_dir_valid,fname), os.path.join(image_dir_valid, fname))
-    elif fname.endswith("_mask.png"):
-        shutil.move(os.path.join(source_dir_valid, fname), os.path.join(mask_dir_valid, fname))
 
